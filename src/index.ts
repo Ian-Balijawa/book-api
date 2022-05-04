70a65520becf732aea3e7416e0b1404aae43e640
ian <ianbalijawa16@gmail.com>
2022-05-04 08:46:16 +0300

    (express:server) setup server to work with graphql and mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-async function main(): Promise<string> {
-  console.log('Main')
-  return new Promise((r, j) => {
-    setTimeout(() => {
-      r('Hello World!')
-    }, 1000) // 1s
+import express, { Application } from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { schema } from './schema/schema'
+import mongoose from 'mongoose'
+
+const app: Application = express()
+
+const mongoUri = 'mongodb://localhost:27017/books'
+
+async function connectToDatabase() {
+  await mongoose.connect(mongoUri)
+  console.log('Connected to mongoDB via: ' + mongoUri)
+}
+
+app.use(
+  '/api',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV !== 'production'
   })
-}
+)
 
-async function runMain() {
-  const Hello = await main()
-  return Hello
-}
+connectToDatabase()
 
-runMain()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
