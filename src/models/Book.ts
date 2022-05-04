420f0b946e40e6189aa669546c9cbf6245abcbf2
ian <ianbalijawa16@gmail.com>
2022-05-04 08:41:41 +0300

    (DB:model) created a Book model

---
+++ src/models/Book.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose'
+import { AuthorDoc } from './Author'
+
+interface BookAttrs {
+  name: string
+  author: AuthorDoc
+  genre: string
+}
+
+interface BookDoc extends mongoose.Document {
+  name: string
+  genre: string
+  author: AuthorDoc
+}
+
+interface BookModel extends mongoose.Model<BookDoc> {
+  build(attrs: BookAttrs): BookDoc
+}
+
+const bookSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    genre: {
+      type: String,
+      required: true
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Author'
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.__v
+      }
+    }
+  }
+)
+
+bookSchema.statics.build = (attrs: BookAttrs) => {
+  return new Book(attrs)
+}
+
+const Book = mongoose.model<BookDoc, BookModel>('Book', bookSchema)
+
+export { Book }
